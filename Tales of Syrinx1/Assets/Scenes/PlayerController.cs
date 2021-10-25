6d64e4457a4ce70614b82cede911eadbfa6193b4
Alt3rmis <18111272170@163.com>
2021-10-25 11:43:10 +0100

    Added playercontroller
    add playercontroller

--- Tales of Syrinx1/Assets/Scenes/PlayerController.cs
+++ Tales of Syrinx1/Assets/Scenes/PlayerController.cs
@@ -12,14 +12,27 @@ public class PlayerController : MonoBehaviour
         rg = this.GetComponent<Rigidbody>();
     }
     void  Update () {
-        OnMove();
+        //OnMove();
         FixedUpdate();
     }
-    void OnMove(InputValue inputValue)
-    {
-        newPosition = rg.position + new Vector3(inputValue.Get<Vector2>().x * speed, 0f, inputValue.Get<Vector2>().y * speed);
 
+    public void OnMovement(InputAction.CallbackContext value)
+    {
+        Vector2 inputMovement = value.ReadValue<vector2>();
+        rawInputMovement = new Vector3(inputMovement.x, 0, inputMovement.y);
     }
+
+    public void OnAttack(InputAction.CallbackContext value)
+    {
+        if (value.started)
+        {
+            Debug.Log("Att");
+        }
+    }
+    //void OnMove(InputValue inputValue)
+    //{
+    //newPosition = rg.position + new Vector3(inputValue.Get<Vector2>().x * speed, 0f, inputValue.Get<Vector2>().y * speed);
+    //}
 
     // Update is called once per frame
     void FixedUpdate()
