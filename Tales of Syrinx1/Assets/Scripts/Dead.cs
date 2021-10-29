c0107830556891271edc123f764eed8b830b9a27
NoFaceGoose <c.peng@hss20.qmul.ac.uk>
2021-10-29 12:03:50 +0100

    new

--- Tales of Syrinx1/Assets/Scripts/Dead.cs
+++ Tales of Syrinx1/Assets/Scripts/Dead.cs
@@ -7,17 +7,18 @@ public class Dead : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-     
+
     }
 
     // Update is called once per frame
     void Update()
     {
-       
+
     }
 
-    void OnTriggerEnter(Collider other){
-    Destroy(gameObject);
-}
+    void OnTriggerEnter(Collider other)
+    {
+        Destroy(other.gameObject);
+    }
 
 }
